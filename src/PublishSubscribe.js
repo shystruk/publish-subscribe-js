94c31b94a6dad1ebbb06ef0f7c0eb5c8fa41f4be
shystruk <v.stokol@gmail.com>
2020-12-12 10:30:15 -0800

    formatting

--- src/PublishSubscribe.js
+++ src/PublishSubscribe.js
@@ -3,80 +3,78 @@
  License: MIT - https://opensource.org/licenses/MIT
  https://github.com/shystruk/publish-subscribe-js
  */
-(function(f) {
-    if (typeof exports === 'object' && typeof module !== 'undefined') {
-        module.exports = f();
-    } else if (typeof define === 'function' && define.amd) {
-        define([],f);
-    } else {
-        var g;
+(function (f) {
+	if (typeof exports === 'object' && typeof module !== 'undefined') {
+		module.exports = f();
+	} else if (typeof define === 'function' && define.amd) {
+		define([], f);
+	} else {
+		var g;
 
-        if (typeof window !== 'undefined') {
-            g = window;
-        } else if(typeof global !== 'undefined') {
-            g = global;
-        } else if(typeof self !== 'undefined') {
-            g = self;
-        } else {
-            g = this;
-        }
+		if (typeof window !== 'undefined') {
+			g = window;
+		} else if (typeof global !== 'undefined') {
+			g = global;
+		} else if (typeof self !== 'undefined') {
+			g = self;
+		} else {
+			g = this;
+		}
 
-        g.myModule = f();
-    }
-})(function() {
-    var define,module,exports;
+		g.myModule = f();
+	}
+})(function () {
+	return {
+		key: 0,
+		subscribers: {},
 
-    return {
-        key: 0,
-        subscribers: {},
+		/**
+		 * @param {String} topic
+		 * @param {Function} subscriber
+		 * @return {Number}
+		 */
+		subscribe: function (topic, subscriber) {
+			if (topic in this.subscribers) {
+				this.subscribers[topic][this.key] = subscriber;
+			} else {
+				this.subscribers[topic] = {};
+				this.subscribers[topic][this.key] = subscriber;
+			}
 
-        /**
-         * @param {String} topic
-         * @param {Function} subscriber
-         * @return {Number}
-         */
-        subscribe: function (topic, subscriber) {
-            if (topic in this.subscribers) {
-                this.subscribers[topic][this.key] = subscriber;
-            } else {
-                this.subscribers[topic] = {};
-                this.subscribers[topic][this.key] = subscriber;
-            }
+			return this.key++;
+		},
 
-            return this.key++;
-        },
+		/**
+		 * @param {String} topic
+		 * @param {*=} data
+		 */
+		publish: function (topic, data) {
+			for (var sub in this.subscribers[topic]) {
+				try {
+					this.subscribers[topic][sub](data, Array.prototype.slice.call(arguments));
+				} catch (e) {
+					console.log(
+						Error('Issue with subscriber ' + topic + '. ' + (e && e.message))
+					);
+				}
+			}
+		},
 
-        /**
-         * @param {String} topic
-         * @param {*=} data
-         */
-        publish: function (topic, data) {
-            for (var sub in this.subscribers[topic]) {
-                try {
-                    this.subscribers[topic][sub](data, Array.prototype.slice.call(arguments));
-                } catch (e) {
-                    console.log(
-                        Error('Issue with subscriber ' + topic + '. ' + (e && e.message))
-                    );
-                }
-            }
-        },
+		/**
+		 * @param {String} topic
+		 * @param {Number} key
+		 */
+		unsubscribe: function (topic, key) {
+			if (key) {
+				delete this.subscribers[topic][key];
+			} else {
+				delete this.subscribers[topic];
+			}
+		},
 
-        /**
-         * @param {String} topic
-         * @param {Number} key
-         */
-        unsubscribe: function (topic, key) {
-            if (key) {
-                delete this.subscribers[topic][key];
-            } else {
-                delete this.subscribers[topic];
-            }
-        },
-
-        unsubscribeAll: function () {
-            this.subscribers = {};
-            this.key = 0;
-        }
-    };
+		unsubscribeAll: function () {
+			this.subscribers = {};
+			this.key = 0;
+		}
+	};
 });
--- test.js
+++ test.js
@@ -1,41 +1,39 @@
 import test from 'ava';
-import PublishSubscribe  from './src/PublishSubscribe';
+import PublishSubscribe from './src/PublishSubscribe';
 
 test.beforeEach(t => {
-    PublishSubscribe.key = 0;
-    PublishSubscribe.unsubscribeAll();
+	PublishSubscribe.key = 0;
+	PublishSubscribe.unsubscribeAll();
 });
 
 test('by default key should be 0', t => {
-    t.is(PublishSubscribe.key, 0);
+	t.is(PublishSubscribe.key, 0);
 });
 
 test('subscribe and publish to the TOPIC', t => {
-    const callback = (data) => {
-        t.deepEqual(data, {message: 'Hello World!'});
-    };
+	const callback = (data) => {
+		t.deepEqual(data, { message: 'Hello World!' });
+	};
 
-    PublishSubscribe.subscribe('TOPIC', callback);
-    t.is(PublishSubscribe.key, 1);
+	PublishSubscribe.subscribe('TOPIC', callback);
+	t.is(PublishSubscribe.key, 1);
 
-    PublishSubscribe.publish('TOPIC', {message: 'Hello World!'});
+	PublishSubscribe.publish('TOPIC', { message: 'Hello World!' });
 });
 
 test('unsubscribe TOPIC', t => {
-    PublishSubscribe.subscribe('TOPIC', function () {});
-    PublishSubscribe.unsubscribe('TOPIC');
-    t.is(PublishSubscribe.key, 1);
-    t.deepEqual(PublishSubscribe.subscribers, {});
+	PublishSubscribe.subscribe('TOPIC', function () { });
+	PublishSubscribe.unsubscribe('TOPIC');
+	t.is(PublishSubscribe.key, 1);
+	t.deepEqual(PublishSubscribe.subscribers, {});
 });
 
 test('unsubscribeAll TOPICS', t => {
-    PublishSubscribe.subscribe('TOPIC', function () {});
-    PublishSubscribe.subscribe('TOPIC', function () {});
-    t.is(PublishSubscribe.key, 2);
+	PublishSubscribe.subscribe('TOPIC', function () { });
+	PublishSubscribe.subscribe('TOPIC', function () { });
+	t.is(PublishSubscribe.key, 2);
 
-    PublishSubscribe.unsubscribeAll();
-    t.deepEqual(PublishSubscribe.subscribers, {});
-    t.is(PublishSubscribe.key, 0);
+	PublishSubscribe.unsubscribeAll();
+	t.deepEqual(PublishSubscribe.subscribers, {});
+	t.is(PublishSubscribe.key, 0);
 });
-
-
